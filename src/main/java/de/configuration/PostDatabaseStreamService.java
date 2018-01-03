a7fcca938d28d63dc3f92a39ad3455fab4cf0d72
simon0420 <simon.bec@live.de>
2018-01-03 20:45:34 +0100

    changes

---
+++ src/main/java/de/configuration/PostDatabaseStreamService.java
@@ -0,0 +1,46 @@
+package de.configuration;
+
+import de.domains.domainAux.Route;
+import de.repositories.postDBRepositories.RouteRepository;
+import de.services.SpotService;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class PostDatabaseStreamService {
+
+    @Autowired
+    RouteRepository routes;
+
+    SpotService s = new SpotService();
+
+    @Autowired
+    public AmqpTemplate template;
+
+    @Autowired
+    private FanoutExchange fanout;
+
+    /**
+     * Method being called whenever the FanoutExchange (connected to the PostDatabaseStreamService class) is triggered.
+     * @param message
+     */
+    public void postDatabaseStreamRoute(Long message){
+        template.convertAndSend(fanout.getName(),"", message);
+    }
+
+    /**
+     * Method being called whenever the FanoutExchange (connected to the PostDatabaseStreamService class) is triggered.
+     * @param message
+     */
+    public void postDatabaseStreamMessage(Long message){
+
+        Route r = routes.getRoute(message);
+        s.learningSpotStructure(r);
+
+    }
+
+}
